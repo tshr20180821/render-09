421e8a61314fdb99d7ff4f105dc95a4c2354e684
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-28 21:40:54 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+curl -LO https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
+
+gunzip gost-linux-amd64-2.11.5.gz
+
+chmod +x gost-linux-amd64-2.11.5
+
+./gost-linux-amd64-2.11.5 -L=:8080
