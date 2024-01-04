b8fb4954ff806df8651d2fb9a8a0a05f350f8a4f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-05 07:25:26 +0900

    Create build_memcached.sh

---
+++ build_memcached.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+pushd /tmp
+
+curl -O https://memcached.org/files/memcached-1.6.22.tar.gz
+
+tar xf memcached-1.6.22.tar.gz
+
+pushd memcached-1.6.22
+
+./configure --help
+
+# ./configure --enable-sasl --enable-sasl-pwdb --enable-static --enable-64bit --disable-docs
+./configure --enable-sasl --enable-sasl-pwdb --enable-64bit --disable-docs
+
+make
+
+make install
+
+popd
+
+ldd /usr/local/bin/memcached
+cp /usr/local/bin/memcached /var/www/html/
+
+popd
