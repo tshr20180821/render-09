df537f03293a0dd6ca0d2fc25a62c54863047e7c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-05 16:01:19 +0900

    Update distccd_wrapper.sh

--- distccd_wrapper.sh
+++ distccd_wrapper.sh
@@ -10,7 +10,8 @@ echo ${filename} >&2
 
 echo -n "$@" | base64 >${filename}
 
-echo -n $(gzip -c ${filename} | curl -X POST --data-binary @- -H "Content-Encoding: gzip" --compressed https://${RENDER_EXTERNAL_HOSTNAME}/distcc_wrapper.php) | base64 -d
+# echo -n $(gzip -c ${filename} | curl -X POST --data-binary @- -H "Content-Encoding: gzip" --compressed https://${RENDER_EXTERNAL_HOSTNAME}/distcc_wrapper.php) | base64 -d
+curl -X POST --data-binary @${filename} https://${RENDER_EXTERNAL_HOSTNAME}/distcc_wrapper.php) | base64 -d
 
 rm ${filename}
 
