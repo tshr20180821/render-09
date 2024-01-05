3e9849369fc7f36c3c58901ba2eaae09561fbba4
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-05 16:10:11 +0900

    Update distcc_wrapper.php

--- distcc_wrapper.php
+++ distcc_wrapper.php
@@ -5,9 +5,9 @@ error_log("start distcc_wrapper.php");
 $data = file_get_contents('php://input');
 error_log("data start");
 error_log(strlen($data));
-error_log(base64_decode($data));
+error_log(base64_decode(gzdecode($data)));
 error_log("data finish");
-$data = base64_decode($data);
+$data = base64_decode(gzdecode($data));
 
 // exec /usr/bin/distccd --log-level warning --log-file ${DISTCC_LOG} $@
 # $res = shell_exec("exec /usr/bin/distccd --log-level warning --log-file /var/www/html/distcc_log.txt ${data}");
