1c630a31383200ee03886f41a2333601677d6592
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-05 11:02:45 +0900

    Update distccd_wrapper.sh

--- distccd_wrapper.sh
+++ distccd_wrapper.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-filename=$(mktemp $(date +'%Y%m%d%H%M%S').XXXXXX.dat)
+echo "$@"
 
-echo -n "$@" | base64 >${filename}
+# filename=$(mktemp $(date +'%Y%m%d%H%M%S').XXXXXX.dat)
 
-echo -n $(gzip -c ${filename} | curl -X POST --data-binary @- -H "Content-Encoding: gzip" --compressed https://${RENDER_EXTERNAL_HOSTNAME}/distcc_wrapper.php) | base64 -d
+# echo -n "$@" | base64 >${filename}
 
-rm ${filename}
+# echo -n $(gzip -c ${filename} | curl -X POST --data-binary @- -H "Content-Encoding: gzip" --compressed https://${RENDER_EXTERNAL_HOSTNAME}/distcc_wrapper.php) | base64 -d
+
+# rm ${filename}
