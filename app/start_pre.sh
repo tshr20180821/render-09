e3ce404fc2d2bb33d4bb6fe0e735ca4d3f5c5eae
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-10 06:43:58 +0900

    Update start_pre.sh

--- app/start_pre.sh
+++ app/start_pre.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+rm /usr/src/app/start.sh
 curl -L -H 'Cache-Control: no-cache' -o /usr/src/app/start.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/app/start.sh?$(date +%s)
+cat /usr/src/app/start.sh
 chmod +x /usr/src/app/start.sh
 /usr/src/app/start.sh
