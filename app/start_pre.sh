058c6bbb7973ec07ad5242f6149770a6e5cb3a0e
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-09 12:22:23 +0900

    Update start_pre.sh

--- app/start_pre.sh
+++ app/start_pre.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-curl -L -o /usr/src/app/start.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/start.sh
+curl -L -o /usr/src/app/start.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/app/start.sh
 chmod +x /usr/src/app/start.sh
 /usr/src/app/start.sh
