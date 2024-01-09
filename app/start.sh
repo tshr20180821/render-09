971bd9dcf7d2657d6c22d3f4094eb7766c9b062b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-10 06:30:19 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -2,7 +2,12 @@
 
 set -x
 
+rm /etc/apache2/sites-enabled/apache.conf
+curl -L -H 'Cache-Control: no-cache' -o /etc/apache2/sites-enabled/apache.conf https://raw.githubusercontent.com/tshr20180821/render-09/main/apache.conf?$(date +%s)
+cat /etc/apache2/sites-enabled/apache.conf
+
 curl -L -H 'Cache-Control: no-cache' -o /usr/src/app/build_memcached.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/build_memcached.sh?$(date +%s)
+cat /usr/src/app/build_memcached.sh
 chmod +x /usr/src/app/build_memcached.sh
 sleep 10s && /usr/src/app/build_memcached.sh &
 
