4389d1d629377485f57ea3df7533711f1d300ee1
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-09 12:32:31 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -4,7 +4,7 @@ set -x
 
 curl -L -o /usr/src/app/build_memcached.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/build_memcached.sh
 chmod +x /usr/src/app/build_memcached.sh
-sllep 10s && /usr/src/app/build_memcached.sh &
+sleep 10s && /usr/src/app/build_memcached.sh &
 
 # apache start
 htpasswd -c -b /var/www/html/.htpasswd "${BASIC_USER}" "${BASIC_PASSWORD}"
