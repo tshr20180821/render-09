f91410d34b4b445ad6ef7896f51e519c261b47f0
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-09 12:38:59 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -9,10 +9,11 @@ curl -L https://github.com/nwtgck/piping-server-pkg/releases/download/v1.12.9-1/
 # socat TCP-LISTEN:3633,bind=127.0.0.1,reuseaddr,fork 'EXEC:exec /bin/distccd --log-level warning --log-file /var/www/html/auth/distccd_log.txt -'
 # socat 'EXEC:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request!!EXEC:curl -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response' TCP:127.0.0.1:3633
 socat -v 'EXEC:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request!!EXEC:curl -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response' \
-  'EXEC:exec /bin/distccd --log-level info --log-file /var/www/html/auth/distccd_log.txt -'
+  'EXEC:exec /bin/distccd --log-level info --log-file /var/www/html/auth/distccd_log.txt -' &
 
 # client
-socat -x TCP-LISTEN:3632,bind=127.0.0.1,reuseaddr,fork 'EXEC:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response!!EXEC:curl -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request'
+socat -x TCP-LISTEN:3632,bind=127.0.0.1,reuseaddr,fork \
+  'EXEC:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response!!EXEC:curl -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request' &
 
 pushd /tmp
 curl -O https://memcached.org/files/memcached-1.6.22.tar.gz
