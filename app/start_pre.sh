3b2a21bac284d56994acda90de40f27335299fa7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-10 13:40:28 +0900

    Update start_pre.sh

--- app/start_pre.sh
+++ app/start_pre.sh
@@ -14,7 +14,7 @@ echo '<HTML />' >/var/www/html/index.html
 
 
 rm /usr/src/app/start.sh
-curl -L -H 'Cache-Control: no-cache' -o /usr/src/app/start.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/app/start.sh?$(date +%s)
+curl -sSL -H 'Cache-Control: no-cache' -o /usr/src/app/start.sh https://raw.githubusercontent.com/tshr20180821/render-09/main/app/start.sh?$(date +%s)
 cat /usr/src/app/start.sh
 chmod +x /usr/src/app/start.sh
 /usr/src/app/start.sh
