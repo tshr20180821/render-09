d7f0b2d43acd5758103c623d92a5440f4b66afc1
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-10 13:07:37 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -83,12 +83,12 @@ chmod +x piping-duplex
 #   'EXEC:/usr/bin/distccd --user nobody --log-level debug --log-file /var/www/html/auth/distccd_log.txt -' &
 # socat -ddd -vvv "EXEC:curl --http1.1 -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request!!EXEC:curl --http1.1 -vNsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response" \
 #   TCP:127.0.0.1:3634 &
-socat -ddd -v "EXEC:./piping-duplex -s https\://${RENDER_EXTERNAL_HOSTNAME}/piping distccd_request distccd_response" tcp:127.0.0.1:3634
+socat -ddd -v "EXEC:./piping-duplex -c -s https\://${RENDER_EXTERNAL_HOSTNAME}/piping distccd_request distccd_response" tcp:127.0.0.1:3634
 
 # client
 # socat -4 tcp-listen:3632,bind=0.0.0.0,reuseaddr,fork \
 #   "EXEC:curl --http1.1 -vNsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response!!EXEC:curl --http1.1 -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request" &
-socat -4 -x tcp-listen:3632 "EXEC:./piping-duplex -s https\://${RENDER_EXTERNAL_HOSTNAME}/piping distccd_response distccd_request"
+socat -4 -x tcp-listen:3632 "EXEC:./piping-duplex -c -s https\://${RENDER_EXTERNAL_HOSTNAME}/piping distccd_response distccd_request"
 
 # finish socat
 
