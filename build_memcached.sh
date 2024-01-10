924ecdf94440c7adcedd121973efbdb8e1c41c8b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-10 11:29:56 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -74,7 +74,7 @@ ss -ant
 # socat -dd "EXEC:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request!!EXEC:curl -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response" \
 #   'EXEC:/usr/bin/distccd --user nobody --log-level debug --log-file /var/www/html/auth/distccd_log.txt -' &
 socat -ddd "EXEC:curl --http1.1 -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_request!!EXEC:curl --http1.1 -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/distccd_response" \
-  TCP:0.0.0.0:3634 &
+  TCP:127.0.0.1:3634 &
 
 # client
 socat -4 TCP-LISTEN:3632,bind=0.0.0.0,reuseaddr,fork \
