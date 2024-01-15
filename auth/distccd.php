a2db96957cbe1cc84de69e0a9a69a7d3f359469c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-15 16:11:38 +0900

    Add files via upload

---
+++ auth/distccd.php
@@ -0,0 +1,25 @@
+<?php
+
+$data = file_get_contents("php://input");
+
+error_log("Y-m-d H:i:s");
+error_log($data);
+
+$socket = stream_socket_client('tcp://127.0.0.1:13632');
+
+socket_write($socket, $data, strlen($data));
+
+$res = '';
+for (;;) {
+    $buffer = socket_read($socket, 1024);
+    if (strlen($buffer) === 0) {
+        break;
+    }
+    $res .= $buffer;
+}
+
+socket_close($socket);
+
+header('Content-Type: binary/octet-stream');
+
+echo $res;
