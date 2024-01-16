b33ce425e3d197bf91ab59cb1775255b3ce67134
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-16 16:17:33 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 64 | head -n 1)
+KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 16 | head -n 1)
 
 echo 'start curl 1'
 
-echo 'piping_server' | curl -vT - https://ppng.io/${KEYWORD}
+echo 'piping_server' | curl -T - ${PIPING_SERVER}/${KEYWORD}
 
 echo 'finish curl 1'
 
 echo 'start curl 2'
 
-curl https://ppng.io/${KEYWORD}
+curl ${PIPING_SERVER}/${KEYWORD}
 
 echo 'finish curl 2'
