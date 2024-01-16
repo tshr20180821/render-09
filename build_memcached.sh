a70a122c6637e4cba81a3071bba7280005abfb27
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-16 15:56:45 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -6,12 +6,12 @@ KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 64 | head -n 1)
 
 echo 'start curl 1'
 
-curl -X POST -d 'post_piping_server' https://piping-47q675ro2guv.runkit.sh//${KEYWORD}
+curl -vL -X POST -d 'post_piping_server' https://piping.glitch.me/${KEYWORD}
 
 echo 'finish curl 1'
 
 echo 'start curl 2'
 
-curl https://piping-47q675ro2guv.runkit.sh/${KEYWORD}
+curl https://piping.glitch.me/${KEYWORD}
 
 echo 'finish curl 2'
