e26628b2e8a47b8977a94c8fa0cf58f8ce9322a5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-17 16:14:54 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -31,9 +31,9 @@ curl -sSL https://github.com/nwtgck/piping-server-pkg/releases/download/v1.12.9-
 
 sleep 3s
 
-socat "exec:curl --http2 -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}xxx!!exec:curl -NsS --http2 --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}yyy" tcp:127.0.0.1:13632 &
+socat "exec:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}xxx!!exec:curl -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}yyy" tcp:127.0.0.1:13632 &
 
-socat tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork "exec:curl --http2 -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}yyy!!exec:curl --http2 -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}xxx" &
+socat tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork "exec:curl -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}yyy!!exec:curl -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}xxx" &
 
 sleep 3s
 ss -anpt
