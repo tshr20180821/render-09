cec4301df02da251fe6242fbe33480afa8bff3b0
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-17 15:54:27 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -42,8 +42,7 @@ apt-get install -y libevent-dev >/dev/null 2>&1
 
 gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )//g' >/tmp/cflags_option
 cflags_option=$(cat /tmp/cflags_option)
-# export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
-export CFLAGS="-O2 -pipe -fomit-frame-pointer"
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
