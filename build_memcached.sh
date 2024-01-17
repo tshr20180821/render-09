56a661d0311dcec131e6fad2b63fadf201db6291
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-17 10:10:29 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -53,6 +53,8 @@ tar xf memcached-1.6.22.tar.gz
 # export DISTCC_HOSTS="127.0.0.1/1,cpp,lzo localhost/1"
 export DISTCC_HOSTS="127.0.0.1"
 export DISTCC_POTENTIAL_HOSTS="${DISTCC_HOSTS}"
+export DISTCC_FALLBACK=0
+export DISTCC_TCP_CORK=0
 
 pushd memcached-1.6.22
 
