653f51538ac6bf49db57d251f4343078d4e040d9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-18 17:10:15 +0900

    Add files via upload

--- auth/socat.php
+++ auth/socat.php
@@ -9,4 +9,4 @@ if (strlen($keyword) != 64) {
 
 error_log("${server} ${keyword}");
 
-exec('timeout -sKILL 60 | socat "exec:curl -u ' . "${BASIC_USER}:${BASIC_PASSWORD} https://${server}/${keyword}req | base64 -d" . '!!exec:base64 -w0 | curl -u ' . "${BASIC_USER}:${BASIC_PASSWORD} -sST - https://${server}/${keyword}res" . '" tcp:127.0.0.1:13632');
+exec('timeout -sKILL 60 socat "' . "exec:server=${server} keyword=${keyword} /usr/src/app/receive.sh!!server=${server} keyword=${keyword} /usr/src/app/send.sh" . '" tcp:127.0.0.1:13632');
