3c79053d13ed3d8d6551474cec959e12787ac4b9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-21 01:54:54 +0900

    Update distccd.php

--- auth/distccd.php
+++ auth/distccd.php
@@ -6,13 +6,23 @@ $data = file_get_contents("php://input");
 
 error_log($data);
 
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 010');
+
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 020');
+
 socket_connect($socket, '127.0.0.1', 13632);
+
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 030');
 
 socket_write($socket, $data);
 
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 040');
+
 $res = '';
 for (;;) {
+    error_log(date("Y-m-d H:i:s") . ' distccd.php check point 050');
     $buffer = socket_read($socket, 1024);
     if (strlen($buffer) === 0) {
         break;
@@ -20,8 +30,14 @@ for (;;) {
     $res .= $buffer;
 }
 
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 060');
+
 socket_close($socket);
 
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 070');
+
 header('Content-Type: binary/octet-stream');
+
+error_log(date("Y-m-d H:i:s") . ' distccd.php check point 080');
 
 echo $res;
