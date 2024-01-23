35bb64c01a7a8fcfe16c53749a566af92dfaca4f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-23 11:33:58 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -2,31 +2,8 @@
 
 set -x
 
-curl -sSL -H 'Cache-Control: no-cache' -o /var/www/html/auth/distccd.php https://github.com/tshr20180821/render-09/raw/main/auth/send.php
-cat /var/www/html/auth/send.php
-
-DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends \
-  build-essential \
-  distcc \
-  gcc-x86-64-linux-gnu \
-  socat \
-  >/dev/null
-
-DISTCCD_LOG_FILE=/var/www/html/auth/distccd_log.txt
-touch ${DISTCCD_LOG_FILE}
-chmod 666 ${DISTCCD_LOG_FILE}
-
-/usr/bin/distccd --port=13632 --listen=127.0.0.1 --user=nobody --jobs=4 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
-
-echo '***** socat *****'
-
-# socat -ddd -b 81920 tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork,sndbuf=81920 \
-#   "exec:php /var/www/html/auth/distccd.php" &
-
 socat -d -b 81920 tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork,sndbuf=81920 \
   "exec:php /var/www/html/auth/send.php" &
-
-echo '***** socat *****'
 
 sleep 3s
 ss -anpt
@@ -43,18 +20,15 @@ pushd /tmp
 curl -sSO https://memcached.org/files/memcached-1.6.22.tar.gz
 tar xf memcached-1.6.22.tar.gz
 
-# export DISTCC_HOSTS="127.0.0.1/1,cpp,lzo localhost/1"
 export DISTCC_HOSTS="127.0.0.1"
 export DISTCC_POTENTIAL_HOSTS="${DISTCC_HOSTS}"
 export DISTCC_FALLBACK=0
-# export DISTCC_TCP_CORK=0
-# export DISTCC_VERBOSE=1
 
 pushd memcached-1.6.22
 
 ./configure --disable-docs >/dev/null
 
-time MAKEFLAGS="CC=distcc\ gcc" make -j2
+time MAKEFLAGS="CC=distcc\ gcc" make -j4
 
 popd
 popd
