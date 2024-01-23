857bdfb30d325b39e02045191c8675470074cd70
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-23 10:25:44 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -2,6 +2,21 @@
 
 set -x
 
+# sasl memcached
+export MEMCACHED_SERVER=127.0.0.1
+export MEMCACHED_PORT=11211
+export MEMCACHED_USER=memcached
+useradd ${MEMCACHED_USER} -G sasl
+export SASL_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 64 | head -n 1)
+echo ${SASL_PASSWORD} | saslpasswd2 -p -a memcached -c memcached
+chown "${MEMCACHED_USER}":memcached /etc/sasldb2
+sasldblistusers2
+export SASL_CONF_PATH=/tmp/memcached.conf
+echo "mech_list: plain" >/tmp/memcached.conf
+memcached --enable-sasl -v -l "${MEMCACHED_SERVER}" -P "${MEMCACHED_PORT}" -B binary -m 32 -t 3 -d -u "${MEMCACHED_USER}" -P /tmp/11211.tmp &
+
+# build script
+
 curl -sSL -H 'Cache-Control: no-cache' -O https://github.com/tshr20180821/render-09/raw/main/build_memcached.sh
 
 cat /usr/src/app/build_memcached.sh
@@ -25,7 +40,7 @@ echo '<HTML />' >/var/www/html/index.html
 a2enmod \
  authz_groupfile
 
-curl -sSL -H 'Cache-Control: no-cache' -o /etc/apache2/sites-enabled/apache.conf https://raw.githubusercontent.com/tshr20180821/render-09/main/apache.conf
+curl -sSL -H 'Cache-Control: no-cache' -o /etc/apache2/sites-enabled/apache.conf https://github.com/tshr20180821/render-09/raw/main/apache.conf
 cat /etc/apache2/sites-enabled/apache.conf
 
 echo "ServerName ${RENDER_EXTERNAL_HOSTNAME}" >/etc/apache2/sites-enabled/server_name.conf
