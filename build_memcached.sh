c10e4e32d18f425f21a390676960e472747edd64
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-29 10:34:45 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -8,7 +8,7 @@ DISTCCD_LOG_FILE=/var/www/html/auth/distccd_log.txt
 touch ${DISTCCD_LOG_FILE}
 chmod 666 ${DISTCCD_LOG_FILE}
 
-/usr/bin/distccd --port=13632 --listen=127.0.0.1 --user=nobody --jobs=4 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180
+/usr/bin/distccd --port=13632 --listen=127.0.0.1 --user=nobody --jobs=$(($(nproc)/2)) --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180
 
 socat -d -b 81920 tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork \
   "exec:php /var/www/html/auth/distcc.php" &
@@ -21,7 +21,7 @@ apt-get install -y libevent-dev >/dev/null 2>&1
 gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )//g' >/tmp/cflags_option
 cflags_option=$(cat /tmp/cflags_option)
 export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
-export CXXFLAGS="$CFLAGS"
+export CXXFLAGS="${CFLAGS}"
 export LDFLAGS="-fuse-ld=gold"
 
 pushd /tmp
