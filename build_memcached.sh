d928ec746c5a992b30097fd983075aaec7a47ed4
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-29 11:57:04 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -8,7 +8,7 @@ DISTCCD_LOG_FILE=/var/www/html/auth/distccd_log.txt
 touch ${DISTCCD_LOG_FILE}
 chmod 666 ${DISTCCD_LOG_FILE}
 
-/usr/bin/distccd --port=13632 --listen=127.0.0.1 --user=nobody --jobs=$(($(nproc)/2)) --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180
+# /usr/bin/distccd --port=13632 --listen=127.0.0.1 --user=nobody --jobs=$(($(nproc)/2)) --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180
 
 socat -d -b 81920 tcp-listen:3632,bind=127.0.0.1,reuseaddr,fork \
   "exec:php /var/www/html/auth/distcc.php" &
@@ -36,7 +36,7 @@ pushd memcached-1.6.22
 
 ./configure --disable-docs >/dev/null
 
-time MAKEFLAGS="CC=distcc\ gcc" make -j3
+time MAKEFLAGS="CC=distcc\ gcc" make -j6
 
 popd
 popd
