f958146998b5ff9638dac9b082f5a64c31139128
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-12 09:41:48 +0900

    Update test20240126.php

--- auth/test20240126.php
+++ auth/test20240126.php
@@ -2,13 +2,13 @@
 
 $pdo = new PDO($_ENV['PDO_PGSQL_DSN']);
 
-$statement_select = $pdo->prepare('SELECT version() VERSION');
+$statement_select = $pdo->prepare('SELECT version() version');
 
 $rc = $statement_select->execute();
 $results = $statement_select->fetchAll();
 
 foreach ($results as $row) {
-    error_log($row['VERSION']);
+    error_log($row['version']);
 }
 
 $pdo = null;
